2c2b3460114f398d9aad06113082170249b00b49
Alex <cota8091@gmail.com>
2025-03-31 22:53:12 +0300

    deployment1

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 'use server';
 
 import { db } from '@/utils/db';
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 // components/TodoList.jsx (Client Component)
 'use client';
 
